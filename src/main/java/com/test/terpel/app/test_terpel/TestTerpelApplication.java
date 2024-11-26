44a85754c0fc6cba7dfe10f3448988732f41b650
Santiago Rodriguez Caceres <santiago.rodriguez02@uptc.edu.co>
2024-11-25 20:54:39 -0500

    Project initialization

---
+++ src/main/java/com/test/terpel/app/test_terpel/TestTerpelApplication.java
@@ -0,0 +1,13 @@
+package com.test.terpel.app.test_terpel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTerpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTerpelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/terpel/app/test_terpel/TestTerpelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.terpel.app.test_terpel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTerpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
